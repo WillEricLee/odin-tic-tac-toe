f2a1464b2944b1639b587f8006316684feb08127
Will Lee <willericlee@gmail.com>
2024-03-24 18:26:53 -0700

    Add marking on click, function closure fix
    More definite closures as I move functions into gameBoard and gameManager
    as necessary.

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 function player(mark, name) {
-    return {mark, name};
+    let wins = 0;
+
+    const getWins = () => wins;
+    const addWin = () => wins++;
+    return {mark, name, getWins, addWin};
 }
 
 const player1 = player('X', 'Xavier');
 const player2 = player('O', 'Orin');
-
-//references to each of the squares
-//I'm getting the references here so that they only have to be made once
-const square00 = document.querySelector(".square00");
-const square01 = document.querySelector(".square01");
-const square02 = document.querySelector(".square02");
-const square10 = document.querySelector(".square10");
-const square11 = document.querySelector(".square11");
-const square12 = document.querySelector(".square12");
-const square20 = document.querySelector(".square20");
-const square21 = document.querySelector(".square21");
-const square22 = document.querySelector(".square22");
 
 const gameBoard = (function() {
     let board = [['N', 'N', 'N'], ['N', 'N', 'N'], ['N', 'N', 'N']]; //initialize blank board
@@ -25,58 +17,66 @@ const gameBoard = (function() {
         (2,0) (2,1) (2,2)
     */
 
-    const makeMove = (letter, x, y) => {
-        board[x][y] = letter;
-        console.log(checkWinner(board));
-        displayBoard(board, [[square00, square01, square02], [square10, square11, square12], [square20, square21, square22]]);
-    };
+    const setMark = (mark, x, y) => board[x][y] = mark;
     const resetBoard = () => board = [['N', 'N', 'N'], ['N', 'N', 'N'], ['N', 'N', 'N']];
     const getRow = (row) => board[row];
+    const checkWinner = () => { //tells if board array is won by 'X', 'O', or 'N' (none)
+        const allEqual = arr => arr.every( v => v === arr[0] )
+    
+        for (let i = 0; i<3; i++) {
+            //if 3-across
+            if (allEqual(board[i]) && board[i][0]!='N') {
+                return board[i][0];
+            }
+            //if 3-down
+            else if (allEqual([board[0][i], board[1][i]], board[2][i]) && board[0][i]!='N') {
+                return board[0][i];
+            }
+        }
+    
+        //if diagonal 00-22
+        if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) && board[0][0]!='N') {
+            return board[1][1];
+        }
+        //if diagonal 02-20
+        else if ((board[0][2] == board[1][1] && board[1][1] == board[2][0]) && board[0][2]!='N') {
+            return board[1][1];
+        }
+    
+        return 'N';
+    }
 
-    return {board, makeMove, resetBoard, getRow};
+    return {board, setMark, resetBoard, getRow, checkWinner};
 })();
 
 const gameManager = (function() {
     const players = [player1, player2];
+    //references to each of the squares
+    //I'm getting the references here so that they only have to be made once
+    //and also preventing them from being global
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener("click", () => {makeMove(square.dataset.x, square.dataset.y)});
+    });
 
-    const playGame = () => {
+    const startGame = () => {
         gameBoard.resetBoard();
+        
+    };
+    const makeMove = (x, y) => {
+        gameBoard.setMark(turn, x, y);
+        gameBoard.checkWinner(gameBoard);
+        displayBoard(squares);
 
-        for (const player of players) {
-            //regular input
-        }
-    }
+        //swap turn upon completion
+        if (turn == 'X') {turn = 'O';}
+        else {turn = 'X';}
+    };
 
-    return {players, playGame};
+    return {players, startGame, makeMove};
 })();
 
-function checkWinner(board) { //tells if board array is won by 'X', 'O', or 'N' (none)
-    const allEqual = arr => arr.every( v => v === arr[0] )
-
-    for (let i = 0; i<3; i++) {
-        //if 3-across
-        if (allEqual(board[i]) && board[i][0]!='N') {
-            return board[i][0];
-        }
-        //if 3-down
-        else if (allEqual([board[0][i], board[1][i]], board[2][i]) && board[0][i]!='N') {
-            return board[0][i];
-        }
-    }
-
-    //if diagonal 00-22
-    if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) && board[0][0]!='N') {
-        return board[1][1];
-    }
-    //if diagonal 02-20
-    else if ((board[0][2] == board[1][1] && board[1][1] == board[2][0]) && board[0][2]!='N') {
-        return board[1][1];
-    }
-
-    return 'N';
-}
-
-function displayBoard(board, marks) {
+function displayBoard(squares) {
     //display to console
     console.log(gameBoard.getRow(0));
     console.log(gameBoard.getRow(1));
@@ -85,7 +85,9 @@ function displayBoard(board, marks) {
     //loop through board and update corresponding mark divs
     for (let i = 0; i<3; i++) {
         for (let j = 0; j<3; j++) {
-            if (board[i][j] != 'N') {marks[i][j].innerHTML = board[i][j];}
+            if (gameBoard.getRow(i)[j] != 'N') {squares[i*3+j].innerHTML = gameBoard.getRow(i)[j];}
         }
     }
 }
+
+gameManager.startGame();
